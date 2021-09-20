7eea3c3f06abf8b96e4f131836c7351d9364ed70
eigen <jingyu.pang@tufts.edu>
2021-09-19 20:22:00 -0400

    build google auth

---
+++ services/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);  // this id is generated automatically in the mongodb
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(
+    user => {
+      done(null, user);
+    });
+})
+
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId : profile.id})
+          
+        if (existingUser) {
+          return done(null, existingUser);
+        } 
+        const user = await new User({googleId: profile.id}).save()
+        done(null, user); 
+      }
+    )
+  );
