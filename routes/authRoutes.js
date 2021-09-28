381772db619a5ea604be108a7dc8f8506be3aad0
eigen <jingyu.pang@tufts.edu>
2021-09-27 22:52:26 -0400

    set up the react fontend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,11 +8,14 @@ module.exports = app => {
         })
       );
       
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get('/auth/google/callback', passport.authenticate('google'), 
+      (req, res) => {
+      res.redirect('/surveys');
+    });
     
     app.get('/api/logout', (req, res) => {
       req.logout(); // takes the cookie and kills it.
-      res.send(req.user); // we should see empty
+      res.redirect('/');
     });
 
     app.get('/api/current_user', (req, res) => {
